cc40c96921b993069800e34550bad0b603c27dbb
Huy Nguyen <allforone1511@gmail.com>
2014-09-29 20:04:30 +0300

    Implement good suffix shift rule.

--- boyer_moore_algorithm/src/BoyerMooreAlgorithm.java
+++ boyer_moore_algorithm/src/BoyerMooreAlgorithm.java
@@ -1,17 +1,69 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by nguyenthanhhuy on 9/29/14.
  */
 public class BoyerMooreAlgorithm {
+
+    public static class Result {
+        public ArrayList<Integer> matchPositions;
+        public int numOfComparisions;
+
+        public Result(ArrayList<Integer> matchPositions, int numOfComparisions) {
+            this.matchPositions = matchPositions;
+            this.numOfComparisions = numOfComparisions;
+        }
+    }
+
     private String pattern;
-    // Pattern length
+    /**
+     * Pattern length
+     */
     private int m;
-    private HashMap<Character, Integer> rightMostOccurrences;
+    /**
+     * A map contains the right most occurrence of each character in the pattern.
+     */
+    private Map<Character, Integer> rightMostOccurrences;
+    /**
+     * A map contains value (<code>j</code>) for each position key (<code>i</code>),
+     * such that <code>j</code> is the right end position of the right most copy
+     * of pattern[i..m], which is not a suffix of pattern, and is not preceded by pattern[i-1].
+     */
+    private Map<Integer, Integer> rightMostSuffixes;
+    /**
+     * A map contains value (<code>j</code>) for each position key (<code>i</code>),
+     * such that <code>j</code> is the length of the largest suffix of pattern[i..m]
+     * that is also a prefix of pattern.
+     */
+    private Map<Integer, Integer> largestPrefixes;
 
     public BoyerMooreAlgorithm(String pattern) {
         this.pattern = pattern;
         processPattern();
+    }
+
+    public Result match(String text) {
+        ArrayList<Integer> matchPositions = new ArrayList<Integer>();
+        int numOfComparisions = 0;
+        int k = m;
+        int n = text.length();
+        while (k <= n) {
+            int i = m;
+            int h = k;
+            for (; i > 0 && pattern.charAt(i - 1) == text.charAt(h - 1); --i, --h) {
+                ++numOfComparisions;
+            }
+            if (i == 0) {
+                matchPositions.add(k);
+                k += m - largestPrefixes.get(2);
+            } else {
+                ++numOfComparisions;
+                k += Math.max(getBadCharacterShift(i, text.charAt(h - 1)), getGoodSuffixShift(i));
+            }
+        }
+        return new Result(matchPositions, numOfComparisions);
     }
 
     ////////////////////////////////////////////////////////////////////////////
@@ -20,6 +72,9 @@ public class BoyerMooreAlgorithm {
     private void processPattern() {
         m = pattern.length();
         computeRightMostOccurrences();
+        Map<Integer, Integer> commonSuffixes = computeCommonSuffixes();
+        computeRightMostSuffixes(commonSuffixes);
+        computeLargestPrefixes(commonSuffixes);
     }
 
     private void computeRightMostOccurrences() {
@@ -29,6 +84,104 @@ public class BoyerMooreAlgorithm {
             currentChar = pattern.charAt(i - 1);
             rightMostOccurrences.put(currentChar, i);
         }
+    }
+
+    /**
+     * Returns a map contains value <code>l</code> for each position key <code>j</code>,
+     * such that <code>l</code> is the length of the longest common suffix of pattern[1..j]
+     * and pattern itself.
+     * The map can be computed from result returned by {@link BoyerMooreAlgorithm#computeCommonPrefixes(String)}.
+     */
+    private Map<Integer, Integer> computeCommonSuffixes() {
+        Map<Integer, Integer> commonSuffixes = new HashMap<Integer, Integer>();
+        String reversedPattern = new StringBuilder(pattern).reverse().toString();
+        Map<Integer, Integer> commonPrefixes = computeCommonPrefixes(reversedPattern);
+        for (int i : commonPrefixes.keySet()) {
+            // i = m - j + 1
+            int j = m - i + 1;
+            int l = commonPrefixes.get(i);
+            commonSuffixes.put(j, l);
+        }
+        return commonSuffixes;
+    }
+
+    private void computeRightMostSuffixes(Map<Integer, Integer> commonSuffixes) {
+        // rightMostSuffixes.get(i) is largest j such that commonSuffixes.get(j) = m - i + 1.
+        // Thus i = m - commonSuffixes.get(j) + 1
+        rightMostSuffixes = new HashMap<Integer, Integer>();
+        for (int i = 2; i <= m + 1; ++i) {
+            rightMostSuffixes.put(i, 0);
+        }
+        for (int j = 1; j <= m - 1; ++j) {
+            int i = m - commonSuffixes.get(j) + 1;
+            rightMostSuffixes.put(i, j);
+        }
+    }
+
+    private void computeLargestPrefixes(Map<Integer, Integer> commonSuffixes) {
+        // largestPrefixes.get(i) is the largest j <= |pattern[i..m]| such that commonSuffixes.get(j) = j
+        // j <= m - i + 1
+        // Thus i <= m - j + 1
+        largestPrefixes = new HashMap<Integer, Integer>();
+        for (int i = 2; i <= m + 1; ++i) {
+            largestPrefixes.put(i, 0);
+        }
+        for (int j = 1; j <= m - 1; ++j) {
+            if (commonSuffixes.get(j) == j) {
+                for (int i = 0; i <= m - j + 1; ++i) {
+                    largestPrefixes.put(i, j);
+                }
+            }
+        }
+    }
+
+    /**
+     * Returns a map contains value (<code>l</code>) for each position key (<code>j</code>),
+     * such that <code>l</code> is the length of the longest common prefix of pattern[j..m]
+     * and pattern itself.
+     * @param pattern the pattern to be scanned.
+     */
+    private static Map<Integer, Integer> computeCommonPrefixes(String pattern) {
+        int m = pattern.length();
+        Map<Integer, Integer> prefixes = new HashMap<Integer, Integer>();
+        prefixes.put(1, 0);
+        int s = 1;
+        for (int j = 2; j <= m; ++j) {
+            int k = j - s +1;
+            int r = s + prefixes.get(s) - 1;
+            if (r < j) {
+                prefixes.put(j, naiveScan(pattern, j, 1));
+                if (prefixes.get(j) > 0) {
+                    s = j;
+                }
+            } else if (prefixes.get(k) + k < prefixes.get(s)) {
+                prefixes.put(j, prefixes.get(k));
+            } else {
+                int x = naiveScan(pattern, r + 1, r - j + 2);
+                prefixes.put(j, r - j + 1 + x);
+                s = j;
+            }
+        }
+        prefixes.put(1, m);
+        return prefixes;
+    }
+
+    /**
+     * Returns length of the longest match between pattern's sub-strings
+     * begin at two given positions.
+     * @param pattern the pattern to be scanned.
+     * @param p first position of first sub-string
+     * @param q first position of second sub-string
+     */
+    private static int naiveScan(String pattern, int p, int q) {
+        int m = pattern.length();
+        int result = 0;
+        for (; p <= m && q <= m; ++p, ++q, ++result) {
+            if (pattern.charAt(p - 1) != pattern.charAt(q -1)) {
+                break;
+            }
+        }
+        return result;
     }
 
     ////////////////////////////////////////////////////////////////////////////
@@ -46,17 +199,15 @@ public class BoyerMooreAlgorithm {
      * or <code>i</code> if k = 0.
      */
     public int getBadCharacterShift(int i, char b) {
-        return Math.max(1, i - getRightMostOcurrence(b));
+        return Math.max(1, i - getRightMostOccurrence(b));
     }
 
     /**
      * Returns the right most occurrence of a character <code>c</code> in the pattern,
      * or <code>0</code> if the pattern does not contain the character.
      * @param c the character in question.
-     * @return the right most position of <code>c</code> in the pattern,
-     * or <code>0</code> if <code>c</code> does not occur in the pattern.
      */
-    private int getRightMostOcurrence(char c) {
+    private int getRightMostOccurrence(char c) {
         Integer rightMostOcurrence = rightMostOccurrences.get(c);
         return rightMostOcurrence != null ? rightMostOcurrence : 0;
     }
@@ -64,5 +215,20 @@ public class BoyerMooreAlgorithm {
     ////////////////////////////////////////////////////////////////////////////
     // Good suffix shift rule
     ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Returns the longest shift possible when during an attempt at position <code>j</code> in text,
+     * there is an occurrence of pattern[i..m] but there is a mismatch between
+     * <code>pattern[i - 1]</code> and <code>text[i + j -1]</code>.
+     * @param i
+     */
+    public int getGoodSuffixShift(int i) {
+        if (i == m) {
+            return 1;
+        }
+        if (rightMostSuffixes.get(i) > 0) {
+            return m - rightMostSuffixes.get(i);
+        }
+        return m - largestPrefixes.get(i);
+    }
 
 }
--- boyer_moore_algorithm/tests/BoyerMooreAlgorithmTest.java
+++ boyer_moore_algorithm/tests/BoyerMooreAlgorithmTest.java
@@ -7,10 +7,30 @@ import static org.junit.Assert.assertEquals;
 public class BoyerMooreAlgorithmTest {
 
     @Test
+    public void match_correctData() {
+        String text = "Editor's note: Scott Hubbard is director of the Stanford Center of Excellence for Commercial Space Transportation. He is a former director of the NASA Ames Research Center and the author of \"Exploring Mars: Chronicles from a Decade of Discovery.\" He serves as the chair of the SpaceX Commercial Crew Safety Advisory Panel. The views expressed are solely his own.\n" +
+                "(CNN) -- Today's selection of Boeing and SpaceX as the providers of a U.S.-based capability to take humans to the International Space Station (ISS) is a major milestone in the almost six-decade history of space exploration. It is just the latest sign that the old paradigm of government-only space travel is being replaced by something else -- a new business ecosystem composed of novel relationships among NASA and the aerospace industry.\n" +
+                "\n" +
+                "No longer will NASA own the ISS \"trucking company\" -- specifying every nut and bolt. Instead, NASA is buying services from U.S. industry. To be sure, the new announcement made it clear that NASA will be carefully examining the safety aspects of each design. But the designs will still be those of Boeing and SpaceX and vetted by NASA.\n" +
+                "I believe this new approach is America's \"secret weapon\" in what some have described as a space race with China. And, as far as I can tell, while the rest of the world is still stuck in a nearly government-only mode, NASA, with the support of the Obama administration, is letting loose the creativity of American know-how.\n" +
+                " NASA's new unmanned spacecraft NASA gets rare view of black hole NASA to resume launches in the U.S.?\n" +
+                "Beginning with the NASA Commercial Orbital Transportation Services competition, continuing with the Commercial Resupply Services (cargo) and now the Commercial Crew selection, NASA \"bet the farm\" on commercial companies filling the gap left by the retirement of the space shuttle, with the Commercial Crew companies (SpaceX and Boeing) replacing the Russians in bringing NASA astronauts to the ISS. This will allow NASA to invest the savings in deep space capabilities such as SLS and Orion.\n" +
+                "I believe it is critical that both commercial cargo and crew succeed for at least two reasons: First, NASA's proper role can be summed up in three words: \"Explore Deep Space.\" It is time for NASA to turn over the low Earth orbit work to industry while NASA focuses on getting humanity to Mars, following in the tracks of robotic rovers Spirit, Opportunity and Curiosity. Second, history teaches us that without a trailing edge of commercial exploitation and profitability, exploration as large scale, routine human endeavor will not succeed.\n" +
+                "\n" +
+                "As an aerospace professional and former NASA executive, I have encountered over the decades many concepts for private space exploration. Until a few years ago, none of these ideas met the sniff test for what I call the \"practical visionary,\" that is, someone capable of seeing a new future, yet solidly grounded in lessons learned. Something was always missing in these early ventures -- either the technical approach required some \"unobtanium\" technology to be invented, the advocate had good ideas but no money or the \"build it and they will come\" philosophy showed total naiveté in business and marketing.\n" +
+                "Today's selection points us in a different direction. As with the early 20th century airmail routes that helped stimulate aviation, NASA's commercial programs are now the anchor tenants in the government transfer of space services to the private sector. This in turn will enable a robust new business enterprise and allow NASA to focus on Mars -- the ultimate target for exploration.";
+//        String pattern = "is just the latest sign";
+        String pattern = "NASA";
+        BoyerMooreAlgorithm algorithm = new BoyerMooreAlgorithm(pattern);
+//        assertEquals(1, algorithm.match(text));
+        assertEquals(20, algorithm.match(text));
+    }
+
+    @Test
     public void getBadCharacterShift_rightMostOccurrenceLessThanMismatchPosition() {
         String pattern = "pedaled";
         BoyerMooreAlgorithm algorithm = new BoyerMooreAlgorithm(pattern);
-        // Right most occurrence of `p` is 1, so shift 6 - 1.
+        // Right most occurrence of `p` is 1, so shift by 6 - 1.
         assertEquals(5, algorithm.getBadCharacterShift(6, 'p'));
     }
 
@@ -18,7 +38,7 @@ public class BoyerMooreAlgorithmTest {
     public void getBadCharacterShift_rightMostOccurrenceEqualsMismatchPosition() {
         String pattern = "pedaled";
         BoyerMooreAlgorithm algorithm = new BoyerMooreAlgorithm(pattern);
-        // Right most occurrence of `p` is 1, so shift 1.
+        // Right most occurrence of `p` is 1, so shift by 1.
         assertEquals(1, algorithm.getBadCharacterShift(1, 'p'));
     }
 
@@ -26,7 +46,7 @@ public class BoyerMooreAlgorithmTest {
     public void getBadCharacterShift_rightMostOccurrenceGreaterThanMismatchPosition() {
         String pattern = "pedaled";
         BoyerMooreAlgorithm algorithm = new BoyerMooreAlgorithm(pattern);
-        // Right most occurrence of `e` is 6, so shift 1.
+        // Right most occurrence of `e` is 6, so shift by 1.
         assertEquals(1, algorithm.getBadCharacterShift(2, 'e'));
     }
 
@@ -43,7 +63,7 @@ public class BoyerMooreAlgorithmTest {
         String pattern = "pedaled";
         int m = pattern.length();
         BoyerMooreAlgorithm algorithm = new BoyerMooreAlgorithm(pattern);
-        // Right most occurrence of `d` in pattern[1..6] is 3, so shift m - 3 = 7 - 3.
+        // Right most occurrence of `d` in pattern[1..6] is 3, so shift by: m - 3 = 7 - 3.
         assertEquals(4, algorithm.getBadCharacterShift(m, 'd'));
     }
 
